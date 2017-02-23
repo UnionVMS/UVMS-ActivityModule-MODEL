e34b71b48de5dbe69be6fcf79f085b4a0598bd0b
emsanerac <Rachana.Sane@arhs-developments.com>
2017-02-23 12:03:53 +0100

    UNIONVMS-1552 : code cleanup

--- src/main/java/eu/europa/ec/fisheries/uvms/activity/model/dto/facatch/FACatchSummaryRecordDTO.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/activity/model/dto/facatch/FACatchSummaryRecordDTO.java
@@ -1,3 +1,16 @@
+/*
+ *
+ * Developed by the European Commission - Directorate General for Maritime Affairs and Fisheries European Union, 2015-2016.
+ *
+ * This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version. The IFDM Suite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
 package eu.europa.ec.fisheries.uvms.activity.model.dto.facatch;
 
 import eu.europa.ec.fisheries.uvms.activity.model.schemas.GroupCriteriaWithValue;
@@ -9,15 +22,7 @@ import java.util.List;
  */
 public class FACatchSummaryRecordDTO {
 
-   // private Date date;
-  /*  private int day;
-    private String month;
-    private int year;
-    private Area area;
-    private String flagState;
-    private String gearType;
-    private String presentation;
-    private String vesselTransportGuid;*/
+
     private  List<GroupCriteriaWithValue> groups;
     private SummaryTableDTO summaryTable;
 
@@ -41,133 +46,5 @@ public class FACatchSummaryRecordDTO {
         this.summaryTable = summaryTable;
     }
 
-  /*  public FACatchSummaryRecordDTO(Area area, String flagState, String gearType, String presentation) {
 
-        this.area = area;
-        this.flagState = flagState;
-        this.gearType = gearType;
-        this.presentation = presentation;
-    }
-
-
-    public String getVesselTransportGuid() {
-        return vesselTransportGuid;
-    }
-
-    public void setVesselTransportGuid(String vesselTransportGuid) {
-        this.vesselTransportGuid = vesselTransportGuid;
-    }
-
-
-    public Area getArea() {
-        return area;
-    }
-
-    public void setArea(Area area) {
-        this.area = area;
-    }
-
-    public String getFlagState() {
-        return flagState;
-    }
-
-    public void setFlagState(String flagState) {
-        this.flagState = flagState;
-    }
-
-    public String getPresentation() {
-        return presentation;
-    }
-
-    public void setPresentation(String presentation) {
-        this.presentation = presentation;
-    }
-
-    public String getGearType() {
-        return gearType;
-    }
-
-    public void setGearType(String gearType) {
-        this.gearType = gearType;
-    }
-
-    public SummaryTableDTO getSummaryTable() {
-        return summaryTable;
-    }
-
-    public void setSummaryTable(SummaryTableDTO summaryTable) {
-        this.summaryTable = summaryTable;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public void setDay(int day) {
-        this.day = day;
-    }
-
-    public String getMonth() {
-        return month;
-    }
-
-    public void setMonth(String month) {
-        this.month = month;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-*/
-    /*@Override
-   public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof FACatchSummaryDTO)) return false;
-
-        FACatchSummaryDTO that = (FACatchSummaryDTO) o;
-
-        if (getDay() != that.getDay()) return false;
-        if (getYear() != that.getYear()) return false;
-        if (getMonth() != null ? !getMonth().equals(that.getMonth()) : that.getMonth() != null) return false;
-        if (getArea() != null ? !getArea().equals(that.getArea()) : that.getArea() != null) return false;
-        if (getFlagState() != null ? !getFlagState().equals(that.getFlagState()) : that.getFlagState() != null)
-            return false;
-        if (getGearType() != null ? !getGearType().equals(that.getGearType()) : that.getGearType() != null)
-            return false;
-        if (getPresentation() != null ? !getPresentation().equals(that.getPresentation()) : that.getPresentation() != null)
-            return false;
-        return getVesselTransportGuid() != null ? getVesselTransportGuid().equals(that.getVesselTransportGuid()) : that.getVesselTransportGuid() == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = getDay();
-        result = 31 * result + (getMonth() != null ? getMonth().hashCode() : 0);
-        result = 31 * result + getYear();
-        result = 31 * result + (getArea() != null ? getArea().hashCode() : 0);
-        result = 31 * result + (getFlagState() != null ? getFlagState().hashCode() : 0);
-        result = 31 * result + (getGearType() != null ? getGearType().hashCode() : 0);
-        result = 31 * result + (getPresentation() != null ? getPresentation().hashCode() : 0);
-        result = 31 * result + (getVesselTransportGuid() != null ? getVesselTransportGuid().hashCode() : 0);
-        return result;
-    }*/
-
-   /* @Override
-    public String toString() {
-        return "FACatchSummaryDTO{" +
-                "day=" + day +
-                ", month='" + month + '\'' +
-                ", year=" + year +
-                ", area=" + area +
-                ", flagState='" + flagState + '\'' +
-                ", gearType='" + gearType + '\'' +
-                ", presentation='" + presentation + '\'' +
-                ", vesselTransportGuid='" + vesselTransportGuid + '\'' +
-                '}';
-    }*/
 }
--- src/main/java/eu/europa/ec/fisheries/uvms/activity/model/dto/facatch/SummaryTableDTO.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/activity/model/dto/facatch/SummaryTableDTO.java
@@ -1,3 +1,16 @@
+
+/*
+ *
+ * Developed by the European Commission - Directorate General for Maritime Affairs and Fisheries European Union, 2015-2016.
+ *
+ * This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version. The IFDM Suite is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
 package eu.europa.ec.fisheries.uvms.activity.model.dto.facatch;
 
 import eu.europa.ec.fisheries.uvms.activity.model.schemas.FishSizeClassEnum;
@@ -10,8 +23,6 @@ import java.util.Map;
  */
 public class SummaryTableDTO {
 
-  // private Map<FishSizeClassEnum,Map<String,Long>> summaryFishSize;
-   // private Map<FaCatchTypeEnum,Map<String,Long>> summaryFaCatchType;
 
     private Map<FishSizeClassEnum,Object> summaryFishSize;
      private Map<FaCatchTypeEnum,Object> summaryFaCatchType;
