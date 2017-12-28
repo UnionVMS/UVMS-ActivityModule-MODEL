8ec9ba2e25994830380b32a658e45848eb38fb07
emrinalgr <rinaldi.gregory@gmail.com>
2017-12-28 12:11:39 +0100

    add mapToSubscriptionRequest

--- src/main/java/eu/europa/ec/fisheries/uvms/activity/model/mapper/ActivityModuleRequestMapper.java
+++ src/main/java/eu/europa/ec/fisheries/uvms/activity/model/mapper/ActivityModuleRequestMapper.java
@@ -11,13 +11,30 @@ details. You should have received a copy of the GNU General Public License along
 
 package eu.europa.ec.fisheries.uvms.activity.model.mapper;
 
-import eu.europa.ec.fisheries.uvms.activity.model.exception.ActivityModelMarshallException;
-import eu.europa.ec.fisheries.uvms.activity.model.schemas.*;
-import un.unece.uncefact.data.standard.unqualifieddatatype._20.IDType;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import eu.europa.ec.fisheries.uvms.activity.model.exception.ActivityModelMarshallException;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.ActivityIDType;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.ActivityModuleMethod;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.ActivityModuleRequest;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.ActivityTableType;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.ActivityUniquinessList;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.FACatchSummaryReportRequest;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.FishingActivityForTripIds;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.FishingTripRequest;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.GetFishingActivitiesForTripRequest;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.GetNonUniqueIdsRequest;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.GroupCriteria;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.ListValueTypeFilter;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.MapToSubscriptionRequest;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.MessageType;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.PluginType;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.SetFLUXFAReportMessageRequest;
+import eu.europa.ec.fisheries.uvms.activity.model.schemas.SingleValueTypeFilter;
+import un.unece.uncefact.data.standard.reusableaggregatebusinessinformationentity._20.FAQuery;
+import un.unece.uncefact.data.standard.unqualifieddatatype._20.IDType;
 
 /**
  * Created by sanera on 06/06/2016.
@@ -101,5 +118,14 @@ public final class ActivityModuleRequestMapper {
         request.setMethod(ActivityModuleMethod.GET_FISHING_ACTIVITY_FOR_TRIPS);
         request.setFaAndTripIds(fishingActivityForTripIds);
         return JAXBMarshaller.marshallJaxBObjectToString(request);
+    }
+
+    public static String mapToSubscriptionRequest(FAQuery faQuery) throws ActivityModelMarshallException {
+        ActivityModuleRequest mapToSubscriptionRequest = new MapToSubscriptionRequest();
+        mapToSubscriptionRequest.setMethod(ActivityModuleMethod.MAP_TO_SUBSCRIPTION_REQUEST);
+        ((MapToSubscriptionRequest)mapToSubscriptionRequest).setMessageType(MessageType.FLUX_FA_QUERY_MESSAGE);
+        String faQueryAsString = JAXBMarshaller.marshallJaxBObjectToString(faQuery);
+        ((MapToSubscriptionRequest)mapToSubscriptionRequest).setRequest(faQueryAsString);
+        return JAXBMarshaller.marshallJaxBObjectToString(mapToSubscriptionRequest);
     }
 }
